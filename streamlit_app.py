88d7991837b7eabf05f5ba45b4decd5a1a2ffb5f
Andy <51281090+brainygrave@users.noreply.github.com>
2023-10-08 00:59:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,106 @@
-
+from streamlit_chat import message, NO_AVATAR
 import streamlit as st
-from streamlit_chat import message
-import requests
-
-st.set_page_config(
-    page_title="Streamlit Chat - Demo",
-    page_icon=":robot:"
-)
-
-API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-headers = {"Authorization": st.secrets['api_key']}
-
-st.header("Shulex")
 
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
+audio_path = "https://docs.google.com/uc?export=open&id=16QSvoLWNxeqco_Wb2JvzaReSAw5ow6Cl"
+img_path = "https://www.groundzeroweb.com/wp-content/uploads/2017/05/Funny-Cat-Memes-11.jpg"
+youtube_embed = '''
+<iframe width="400" height="215" src="https://www.youtube.com/embed/LMQ5Gauy17k" title="YouTube video player" frameborder="0" allow="accelerometer; encrypted-media;"></iframe>
+'''
 
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+markdown = """
+### HTML in markdown is ~quite~ **unsafe**
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
+<blockquote>
+However, if you are in a trusted environment (you trust the markdown). You can use allow_html props to enable support for html.
+</blockquote>
 
-def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
-    return input_text 
+* Lists
+* [ ] todo
+* [x] done
 
+Math:
 
-user_input = get_text()
+Lift($L$) can be determined by Lift Coefficient ($C_L$) like the following
+equation.
 
-if user_input:
-    output = query({
-        "inputs": {
-            "past_user_inputs": st.session_state.past,
-            "generated_responses": st.session_state.generated,
-            "text": user_input,
-        },"parameters": {"repetition_penalty": 1.33},
-    })
+$$
+L = \\frac{1}{2} \\rho v^2 S C_L
+$$
 
+```py
+import streamlit as st
+st.write("Python code block")
+```
+
+~~~js
+console.log("Here is some JavaScript code")
+~~~
+"""
+
+table_markdown = '''
+A Table:
+
+| Feature     | Support              |
+| ----------: | :------------------- |
+| CommonMark  | 100%                 |
+| GFM         | 100% w/ `remark-gfm` |
+'''
+
+long_message = """A chatbot or chatterbot is a software application used to conduct an on-line chat conversation via text or text-to-speech, in lieu of providing direct contact with a live human agent. 
+Designed to convincingly simulate the way a human would behave as a conversational partner, chatbot systems typically require continuous tuning and testing, and many in production remain unable to adequately converse, while none of them can pass the standard Turing test. 
+The term "ChatterBot" was originally coined by Michael Mauldin (creator of the first Verbot) in 1994 to describe these conversational programs.
+
+[streamlit website](https://streamlit.io)
+"""
+
+def on_input_change():
+    user_input = st.session_state.user_input
     st.session_state.past.append(user_input)
-    st.session_state.generated.append(output["generated_text"])
+    st.session_state.generated.append("The messages from Bot\nWith new line")
 
-if st.session_state['generated']:
+def on_btn_click():
+    del st.session_state.past[:]
+    del st.session_state.generated[:]
 
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+if __name__ == '__main__':
+    st.session_state.setdefault(
+        'past', 
+        ["Hey, \nwhat's a chatbot?", 
+        'plan text with line break',
+        'play the song "Dancing Vegetables"', 
+        'show me image of cat', 
+        'and video of it',
+        'show me some markdown sample',
+        'table in markdown']
+    )
 
+    st.session_state.setdefault(
+        'generated', 
+        [{'type': 'normal', 'data': long_message}, 
+        {'type': 'normal', 'data': 'Line 1 \n Line 2 \n Line 3'},
+        {'type': 'normal', 'data': f'<audio controls src="{audio_path}"></audio>'}, 
+        {'type': 'normal', 'data': f'<img width="100%" height="200" src="{img_path}"/>'}, 
+        {'type': 'normal', 'data': f'{youtube_embed}'},
+        {'type': 'normal', 'data': f'{markdown}'},
+        {'type': 'table', 'data': f'{table_markdown}'}]
+    )
+
+    st.title("Chat placeholder")
+
+    chat_placeholder = st.empty()
+
+    with chat_placeholder.container():    
+        for i in range(len(st.session_state['generated'])):                
+            message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
+            message(
+                st.session_state['generated'][i]['data'], 
+                key=f"{i}", 
+                allow_html=True,
+                is_table=st.session_state['generated'][i]['type']=='table',
+                avatar_style=NO_AVATAR
+            )
+        st.button("Clear message", on_click=on_btn_click)
+
+    with st.container():
+        st.text_input("User Input:", on_change=on_input_change, key="user_input")
